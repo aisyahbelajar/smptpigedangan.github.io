727195acf825ce2d13fadf94fb70a5ae519f74a9
aisyah Lailia <aisyahlailia80@gmail.com>
2024-10-08 22:02:07 +0700

    membuat aplikasi website sekolah

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-see": "#0527DE",
+        "blue-soft": "#5f78fb",
+      },
+    },
+  },
+  plugins: [],
+};
